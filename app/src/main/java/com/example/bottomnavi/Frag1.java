434cff6553da392b5eca8e1c8f6554825d15f193
H-E Yang <201921071@sangmyung.kr>
2021-05-03 00:28:43 +0900

    05/03

---
+++ app/src/main/java/com/example/bottomnavi/Frag1.java
@@ -0,0 +1,111 @@
+package com.example.bottomnavi;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+
+
+public class Frag1 extends Fragment {
+    private View view;
+    private DrawerLayout drawerLayout;
+    private View drawerView;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.frag1, container, false);
+
+        return view;
+
+    }
+}
+
+class Frag01 extends AppCompatActivity {
+
+    private DrawerLayout drawerLayout;
+    private View drawerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.frag1);
+
+        drawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
+        drawerView = (View)findViewById(R.id.drawer);
+
+        Button btn_open = (Button)findViewById(R.id.btn_open);
+        btn_open.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                drawerLayout.openDrawer(drawerView);
+            }
+        });
+
+        Button btn_close = (Button)findViewById(R.id.btn_close);
+        btn_close.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                drawerLayout.closeDrawers();
+            }
+        });
+
+        drawerLayout.setDrawerListener(listener);
+        drawerView.setOnTouchListener(new View.OnTouchListener(){
+
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                return true;
+            }
+        });
+
+    }
+
+    DrawerLayout.DrawerListener listener = new DrawerLayout.DrawerListener() {
+        @Override
+        public void onDrawerSlide(@NonNull View drawerView, float slideOffset) {
+
+        }
+
+        @Override
+        public void onDrawerOpened(@NonNull View drawerView) {
+
+        }
+
+        @Override
+        public void onDrawerClosed(@NonNull View drawerView) {
+
+        }
+
+        @Override
+        public void onDrawerStateChanged(int newState) {
+
+        }
+    };
+    public void onClickButton1 (View v){
+        Intent myIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.smu.ac.kr"));
+        startActivity(myIntent);
+    }
+    public void onClickButton2 (View v){
+        Intent myIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.smu.ac.kr/lounge/notice/notice.do"));
+        startActivity(myIntent);
+    }
+    public void onClickButton3 (View v){
+        Intent myIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://ecampus.smu.ac.kr"));
+        startActivity(myIntent);
+    }
+}
+
+
